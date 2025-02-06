ea20b727e07295e90984b8e1b6f581eef31cca6d
itscaiocunha <caio.cunha@sou.unifeob.edu.br>
2025-02-05 23:29:30 -0300

    feat: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: "./",
+  build: {
+    outDir: "dist", // Certifique-se de que a saída está correta
   },
+  server: {
+    fs: {
+      strict: false, // Evita alguns erros com arquivos externos
+    }
+  }
 });
